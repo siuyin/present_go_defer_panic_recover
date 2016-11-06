d74bc337dc2645d1698f3714e0d49c6d7fbd1298
Loh Siu Yin <siuyin@beyondbroadcast.com>
2016-11-06 19:45:17 +0800

    After func call.

--- panic/main.go
+++ panic/main.go
@@ -11,4 +11,5 @@ func onlyPositive(i int) {
 func main() {
 	defer fmt.Println("We seem to have to have come to an end.")
 	onlyPositive(1) // Try changing to -1 // HL
+	fmt.Println("After func call.")
 }
